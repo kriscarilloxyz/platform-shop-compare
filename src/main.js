e9ae12ac7f0a5a4ac09d1ee9947907de0621d819
kriscarilloxyz <kriscarilloxyz@gmail.com>
2020-04-19 00:32:36 +0800

    added firebase and db in Global mixin

--- src/main.js
+++ src/main.js
@@ -4,11 +4,37 @@ import router from './router'
 import store from './store'
 import './registerServiceWorker'
 import vuetify from './plugins/vuetify'
+import firebase from 'firebase'
+import db from './plugins/db'
 
 /** Layouts */
 import Page from './layouts/Page'
 
 Vue.component('page', Page)
+
+/** Global Mixin */
+
+Vue.mixin({
+  data: () => ({
+    // Project Details
+    firebase: firebase,
+    db: db,
+    project: {
+      name: 'Mineta XYZ'
+    },
+    // Global Form Rules
+    formRules: {
+      email: [
+        v => !!v || 'E-mail is required',
+        v => /.+@.+\..+/.test(v) || 'E-mail must be valid'
+      ],
+      password: [
+        v => !!v || 'Password is required',
+        v => (v && v.length >= 8) || 'Password must be higher than 8 characters'
+      ]
+    }
+  })
+})
 
 Vue.config.productionTip = false
 
