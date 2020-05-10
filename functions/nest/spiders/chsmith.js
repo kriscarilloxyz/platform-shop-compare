d662e0af0e2b25ba5860375c9f8a1e873b992dc9
kriscarilloxyz <kriscarilloxyz@gmail.com>
2020-05-10 20:18:58 +0800

    added spider chsmith #1

---
+++ functions/nest/spiders/chsmith.js
@@ -0,0 +1,23 @@
+const { spider } = require('./spider')
+
+async function _chsmith (db, doc) {
+  console.log('[START] https://www.chsmith.com.au/')
+  const $ = await spider.GET(doc.link, true, false)
+  const title = spider.sanitizeText($('#amfpc-product-link').text())
+  let price
+
+  $('span.price').each((index, element) => {
+    const id = $(element).attr('id')
+    const content = $(element).text()
+    if (id && !price) {
+      if (id.includes('product-price')) {
+        price = spider.sanitizePrice(content)
+      }
+    }
+  })
+
+  if (title && price && db) { spider.saveResults(db, doc, title, price) }
+  console.log(`[RESULTS] TITLE: ${title || 'n/a'} | PRICE: ${price || 'n/a'} | DEBUG: ${db ? 'NO' : 'YES'}`)
+}
+
+exports.chsmith = _chsmith
