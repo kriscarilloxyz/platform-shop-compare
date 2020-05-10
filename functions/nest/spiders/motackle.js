30351a9652013d74951e025bd2b5ee424a38062c
kriscarilloxyz <kriscarilloxyz@gmail.com>
2020-05-10 20:45:54 +0800

    added spider motackle #1

---
+++ functions/nest/spiders/motackle.js
@@ -0,0 +1,23 @@
+const { spider } = require('./spider')
+
+async function _motackle (db, doc) {
+  console.log('[START] https://www.motackle.com.au/')
+  const $ = await spider.GET(doc.link)
+  const title = spider.sanitizeText($('span.h1[itemprop="name"]').text())
+  let price
+
+  $('span.price').each((index, element) => {
+    const id = $(element).attr('id')
+    const content = $(element).text()
+    if (id && !price) {
+      if (id.includes('product-price')) {
+        price = spider.sanitizePrice(content)
+      }
+    }
+  })
+
+  if (title && price && db) { spider.saveResults(db, doc, title, price) }
+  console.log(`[RESULTS] TITLE: ${title || 'n/a'} | PRICE: ${price || 'n/a'} | DEBUG: ${db ? 'NO' : 'YES'}`)
+}
+
+exports.motackle = _motackle
