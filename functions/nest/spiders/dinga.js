ad2941cc44418def3b47ac92306c9186e1221203
kriscarilloxyz <kriscarilloxyz@gmail.com>
2020-05-10 20:20:51 +0800

    added spider dinga #1

---
+++ functions/nest/spiders/dinga.js
@@ -0,0 +1,12 @@
+const { spider } = require('./spider')
+
+async function _dinga (db, doc) {
+  console.log('[START] https://slhobie.com.au')
+  const $ = await spider.GET(doc.link, false, true)
+  const title = spider.sanitizeText($('div.product-name > h1[itemprop="name"]').text())
+  const price = spider.sanitizePrice($('span.price[itemprop="price"]').text())
+  if (title && price && db) { spider.saveResults(db, doc, title, price) }
+  console.log(`[RESULTS] TITLE: ${title || 'n/a'} | PRICE: ${price || 'n/a'} | DEBUG: ${db ? 'NO' : 'YES'}`)
+}
+
+exports.dinga = _dinga
