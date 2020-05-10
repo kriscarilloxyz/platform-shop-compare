d84ec13d44625752ed553f97c20586790c3dcfd4
kriscarilloxyz <kriscarilloxyz@gmail.com>
2020-05-10 20:51:11 +0800

    added spider baysports #1

---
+++ functions/nest/spiders/baysports.js
@@ -0,0 +1,13 @@
+const { spider } = require('./spider')
+
+async function _baysports (db, doc) {
+  console.log('[START] https://www.baysports.com.au/')
+  const $ = await spider.GET(doc.link)
+  const title = spider.sanitizeText($('h1[itemprop="name"]').text())
+  const price = spider.sanitizePrice($('span.product-single__price-number.product-single__price-number--sale').text())
+
+  if (title && price && db) { spider.saveResults(db, doc, title, price) }
+  console.log(`[RESULTS] TITLE: ${title || 'n/a'} | PRICE: ${price || 'n/a'} | DEBUG: ${db ? 'NO' : 'YES'}`)
+}
+
+exports.baysports = _baysports
