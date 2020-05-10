a5befd969b89abb3978da6fc1fc67cf6ce7c0b36
kriscarilloxyz <kriscarilloxyz@gmail.com>
2020-05-10 20:21:09 +0800

    added spider slhobie #1

---
+++ functions/nest/spiders/slhobie.js
@@ -0,0 +1,12 @@
+const { spider } = require('./spider')
+
+async function _slhobie (db, doc) {
+  console.log('[START] https://slhobie.com.au')
+  const $ = await spider.GET(doc.link)
+  const title = spider.sanitizeText($('h2[itemprop="headline"]').text())
+  const price = spider.sanitizePrice($('.sale-price').text())
+  if (title && price && db) { if (title && price && db) { spider.saveResults(db, doc, title, price) } }
+  console.log(`[RESULTS] TITLE: ${title || 'n/a'} | PRICE: ${price || 'n/a'} | DEBUG: ${db ? 'NO' : 'YES'}`)
+}
+
+exports.slhobie = _slhobie
