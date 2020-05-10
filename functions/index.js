3de16a2e4460aa7c3757201d5ba53ad322208643
kriscarilloxyz <kriscarilloxyz@gmail.com>
2020-05-10 20:21:38 +0800

    added spider scheduled function in firebase

---
+++ functions/index.js
@@ -0,0 +1,39 @@
+const admin = require('firebase-admin')
+const functions = require('firebase-functions')
+const { spiders } = require('./nest/spiders')
+
+/** Initialize firebase */
+admin.initializeApp()
+const db = admin.firestore()
+
+/** Register scheduled function */
+exports.spiders = functions
+  .pubsub
+  .schedule('every 2 minutes')
+  .onRun(async context => {
+    const query = db.collection('scheduled')
+    const snapshot = await query.get()
+
+    if (!snapshot.empty) {
+      console.log(`[LOG] ${snapshot.length} scheduled jobs.`)
+      const tasks = []
+      const docs = []
+      snapshot.forEach(doc => docs.push({ ...doc.data(), id: doc.id }))
+
+      docs.forEach(doc => {
+        const task = new Promise((resolve, reject) => spiders[doc.spider](db, doc))
+        tasks.push(task)
+      })
+
+      if (tasks.length) {
+        console.log(`[LOG] Running ${tasks.length} tasks.`)
+        await Promise.all(tasks)
+      } else {
+        console.log('[LOG] No tasks to run.')
+      }
+    } else {
+      console.log('[LOG] No scheduled jobs.')
+    }
+
+    return true
+  })
