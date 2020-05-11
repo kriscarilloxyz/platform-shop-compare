d20f51842d999acc92b44b06f70ce66159660275
kriscarilloxyz <kriscarilloxyz@gmail.com>
2020-05-11 13:46:30 +0800

    added spider theboatwarehouse #1

---
+++ functions/nest/spiders/theboatwarehouse.js
@@ -0,0 +1,12 @@
+const { spider } = require('./spider')
+
+async function _theboatwarehouse (db, doc) {
+  console.log('[START] https://theboatwarehouse.com.au')
+  const $ = await spider.GET(doc.link)
+  const title = spider.sanitizeText($('h1.title').text())
+  const price = spider.sanitizePrice($('em.ProductPrice').text())
+  if (title && price && db) { spider.saveResults(db, doc, title, price) }
+  console.log(`[RESULTS] TITLE: ${title || 'n/a'} | PRICE: ${price || 'n/a'} | DEBUG: ${db ? 'NO' : 'YES'}`)
+}
+
+exports.theboatwarehouse = _theboatwarehouse
