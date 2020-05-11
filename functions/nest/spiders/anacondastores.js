4c0724adfbe3e178bef1d6d77c2fc58e37bcf624
kriscarilloxyz <kriscarilloxyz@gmail.com>
2020-05-11 11:10:03 +0800

    added spider anacondastores #1

---
+++ functions/nest/spiders/anacondastores.js
@@ -0,0 +1,12 @@
+const { spider } = require('./spider')
+
+async function _anacondastores (db, doc) {
+  console.log('[START] https://anacondastores.com.au')
+  const $ = await spider.GET(doc.link)
+  const title = spider.sanitizeText($('.pdp-title[itemprop="name"]').text())
+  const price = spider.sanitizePrice($('p.price.price-regular').first().text())
+  if (title && price && db) { spider.saveResults(db, doc, title, price) }
+  console.log(`[RESULTS] TITLE: ${title || 'n/a'} | PRICE: ${price || 'n/a'} | DEBUG: ${db ? 'NO' : 'YES'}`)
+}
+
+exports.anacondastores = _anacondastores
